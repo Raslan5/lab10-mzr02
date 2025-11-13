3aaf731c91f48c41b8d8ef1ea2c064e4fe714b77
Raslan5 <Refez.25@outlook.com>
2025-11-13 22:08:26 +0200

    fix intendation

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,13 @@
 import unittest
 from app import greet
 
+
 class TestApp(unittest.TestCase):
- def test_greet(self):
-  self.assertEqual(greet("World"), "Hello, World from Mohamad Raslan!")
+    def test_greet(self):
+        self.assertEqual(greet("World"), "Hello, World from Mohamad Raslan!")
+  
   
 if __name__ == "__main__":
- unittest.main()
+    unittest.main()
  
  
